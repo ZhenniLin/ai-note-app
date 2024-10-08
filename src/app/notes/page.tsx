6f102e284269945ef8b4a21eea36941b4ea61668
ZHENNI LIN <925881956@qq.com>
2024-10-08 19:35:19 +0200

    clerk-authentication

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "FlowBrain - Notes",
+};
+
+const NotesPage = () => {
+  return <div>here will be your notes</div>;
+};
+
+export default NotesPage;
