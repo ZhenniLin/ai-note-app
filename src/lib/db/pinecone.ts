9d74ae747b075f0c9b90567fa149df54e0501511
ZHENNI LIN <925881956@qq.com>
2024-10-11 20:02:02 +0200

    openai&pinecone-setup

---
+++ src/lib/db/pinecone.ts
@@ -0,0 +1,15 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+
+const apiKey = process.env.PINECONE_API_KEY;
+
+if (!apiKey) {
+  throw Error("PINECONE_API_KEY is not set");
+}
+
+const pinecone = new Pinecone({
+  apiKey,
+  controllerHostUrl:
+    "https://nextjs-ai-note-app-pj169n8.svc.aped-4627-b74a.pinecone.io",
+});
+
+export const notesIndex = pinecone.Index("nextjs-ai-note-app");
