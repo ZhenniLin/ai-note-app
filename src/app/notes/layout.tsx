b9ce45b8c20350403a41b2e41557d293b9f8cef9
ZHENNI LIN <925881956@qq.com>
2024-10-12 19:54:49 +0200

    deploy-1

--- src/app/notes/layout.tsx
+++ src/app/notes/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <main className="p-4 max-w-7xl m-auto">{children}</main>
+      <main className="p-4 max-w-7xl m-auto ">{children}</main>
     </>
   );
 };
